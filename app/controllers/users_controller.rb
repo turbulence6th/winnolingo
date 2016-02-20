24c22fdd807135eb4b015c7f180980b8d9a6279f
oguz <oguz.tanrikulu@metu.edu.tr>
2016-02-20 13:28:29 +0200

    assets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,40 @@
 class UsersController < ApplicationController
-  
   def index
-    
+
   end
-  
+
   def new
-    
+
   end
-  
+
   def create
-    
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.json {
+          render :json => {
+            :success => true
+          }
+        }
+      else
+        format.json {
+          render :json => {
+            :success => false,
+            :errors => @user.errors
+          }
+        }
+      end
+    end
   end
-  
+
   def show
-    
+
   end
-  
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email, :username,
+    :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   
+  has_secure_password
+  
   has_many :requests, :dependent => :destroy
   has_many :suggestions, :dependent => :destroy
   
